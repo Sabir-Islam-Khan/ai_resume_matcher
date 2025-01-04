145b81f70c21bf4d1c8246df69111211dadfb76c
Sabir Islam Khan <sabir.islam14@gmail.com>
2025-01-04 18:56:16 +0600

    change

--- resume_ai.py
+++ resume_ai.py
@@ -12,6 +12,19 @@ from llama_cloud.client import LlamaCloud
 from llama_index.core.prompts import PromptTemplate
 from llama_index.core.async_utils import run_jobs
 import os 
+import asyncio
+
+from llama_index.indices.managed.llama_cloud import LlamaCloudIndex
+from llama_index.indices.managed.llama_cloud import LlamaCloudIndex
+
+from llama_index.core.vector_stores import (
+    MetadataFilter,
+    MetadataFilters,
+    FilterOperator,
+    FilterCondition
+)
+
+
 
 load_dotenv()
 
@@ -147,6 +160,8 @@ async def get_document_upload(documents, llm):
                 }
             )
 
+
+
 async def upload_documents(client, pipeline, documents):
     """Function to upload the documents to the cloud"""
 
@@ -158,7 +173,162 @@ async def upload_documents(client, pipeline, documents):
     documents_upload_objs = await run_jobs(extract_jobs, workers=4)
 
     _ = client.pipelines.create_batch_pipeline_documents(pipeline.id, request=documents_upload_objs)
+    await upload_documents(client, pipeline, documents)
 
 
 
+index = LlamaCloudIndex(
+  name="resume_matching",
+  project_name="Default",
+  organization_id="9eea158a-7ba5-49e8-bff7-3c060289e5f6",
+)
 
+async def get_query_metadata(text):
+    """Function to get the metadata from the given user query"""
+    prompt_template = PromptTemplate("""Generate skills, and country of the education for the given user query.
+
+    Extracted metadata should be from the following items:
+
+    skills: {global_skills}
+    countries: {global_countries}
+    domains: {global_domains}
+    user query:
+
+    {text}""")
+
+    extracted_metadata = await llm.astructured_predict(
+        Metadata,
+        prompt_template,
+        text=text,
+        global_skills=global_skills,
+        global_countries=global_countries,
+        global_domains=global_domains
+    )
+
+    return extracted_metadata
+
+async def candidates_retriever_from_query(query: str):
+    """Synthesizes an answer to your question by feeding in an entire relevant document as context."""
+    print(f"> User query string: {query}")
+    # Use structured predict to infer the metadata filters and query string.
+    metadata_info = await get_query_metadata(query)
+    filters = MetadataFilters(
+    filters=[
+        MetadataFilter(key="domain", operator=FilterOperator.EQ, value=metadata_info.domain),
+        MetadataFilter(key="country", operator=FilterOperator.IN, value=metadata_info.country),
+        MetadataFilter(key="skills", operator=FilterOperator.IN, value=metadata_info.skills)
+    ],
+    condition=FilterCondition.OR
+)
+    print(f"> Inferred filters: {filters.json()}")
+    retriever = index.as_retriever(
+    retrieval_mode="chunks",
+    metadata_filters=filters,
+    )
+    # run query
+    return retriever.retrieve(query)
+
+def get_candidates_file_paths(candidates):
+
+  file_paths = []
+  for candidate in candidates:
+    file_paths.append(candidate.metadata['file_path'])
+
+  return list(set(file_paths))
+
+async def candidates_retriever_from_jd(job_description: str):
+    # Use structured predict to infer the metadata filters and query string.
+    metadata_info = await get_metadata(job_description)
+    filters = MetadataFilters(
+    filters=[
+        MetadataFilter(key="domain", operator=FilterOperator.EQ, value=metadata_info.domain),
+        MetadataFilter(key="country", operator=FilterOperator.IN, value=metadata_info.country),
+        MetadataFilter(key="skills", operator=FilterOperator.IN, value=metadata_info.skills)
+    ],
+    condition=FilterCondition.OR
+)
+    print(f"> Inferred filters: {filters.json()}")
+    retriever = index.as_retriever(
+    retrieval_mode="chunks",
+    metadata_filters=filters,
+    )
+    # run query
+    return retriever.retrieve(job_description)
+
+
+def main():
+    directory = './sampled_data/'
+    pdf_files = list_pdf_files(directory)
+
+    documents = parse_files(pdf_files)
+
+    global_skills = []
+    global_countries = []
+    global_domains = []
+
+    # Embedding config
+    embedding_config = {
+        'type': 'OPENAI_EMBEDDING',
+        'component': {
+            'api_key': os.environ["OPENAI_API_KEY"], # editable
+            'model_name': 'text-embedding-ada-002' # editable
+        }
+    }
+
+    # Transformation auto config
+    transform_config = {
+        'mode': 'auto',
+        'config': {
+            'chunk_size': 1024,
+            'chunk_overlap': 20
+        }
+    }
+
+    client, pipeline = create_llamacloud_pipeline('resume_matching', embedding_config, transform_config)
+
+    index = LlamaCloudIndex(
+        name="resume_matching",
+        project_name="Default",
+        organization_id="9eea158a-7ba5-49e8-bff7-3c060289e5f6",
+    )
+
+    async def run():
+        query = "I want someone who studied in US, Java developer, and worked in IT"
+        nodes = await candidates_retriever_from_query(query)
+        print(get_candidates_file_paths(nodes))
+
+        job_description_file_path = './job_description.pdf'
+
+        job_description_document = parse_files([job_description_file_path])
+
+        job_description = "\n\n".join([doc.text for doc in job_description_document[0]])
+
+        print(job_description)
+
+        candidates_based_on_jd = await candidates_retriever_from_jd(job_description)
+
+        candidates_file_paths = get_candidates_file_paths(candidates_based_on_jd)
+
+        print(candidates_file_paths)
+
+        candidates_resumes = parse_files(candidates_file_paths)
+
+        candidates_resumes_text = "\n\n".join([doc.text for docs in candidates_resumes for doc in docs])
+
+        query = f"""Based on the following job description, please share the analysis of why specific candidates are suitable for the job.
+
+        Job Description:
+        {job_description}
+
+        Candidates:
+        {candidates_resumes_text}
+        """
+
+        analyses = llm.complete(query)
+
+        print(analyses)
+
+    asyncio.run(run())
+
+if __name__ == "__main__":
+    main()
